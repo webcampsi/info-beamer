37aca80056e7096cec126852f7551dc9433a931c
Jure Cuhalev <gandalfar@gmail.com>
2015-03-12 12:55:03 +0100

    schedule shifting script

---
+++ shift-schedule.py
@@ -0,0 +1,12 @@
+import json
+import arrow
+
+schedule = json.loads(open('webcamp-2015/schedule/schedule.json').read())
+for item in schedule:
+    start = arrow.get(item.get('start'))
+    item['start'] = start.replace(days=+1).format('X')
+    
+    end = arrow.get(item.get('end'))
+    item['end'] = end.replace(days=+1).format('X')
+
+print json.dumps(schedule)
