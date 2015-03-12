a162d2a7ede47038728b4ddd600fd7614d55c70b
Goran Blažič <goranche@me.com>
2015-03-12 22:06:29 +0100

    added check-git script

--- run-beamer.sh
+++ run-beamer.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-[ -f /boot/room ] && cat /boot/room > /home/goranb/info-beamer/webcamp-2015/schedule/room
+[ -f /boot/room ] && cp /boot/room /home/goranb/info-beamer/webcamp-2015/schedule/room
 nohup /opt/info-beamer-pi/info-beamer /home/goranb/info-beamer/webcamp-2015 &
