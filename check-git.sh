aa0c6c0bce83e92dc2fe70256faf1407bb37d371
Goran Blažič <goranche@me.com>
2015-03-12 23:08:21 +0100

    fixed git checker...

--- check-git.sh
+++ check-git.sh
@@ -1,17 +1,11 @@
 #!/bin/bash
+
 cd /home/goranb/info-beamer
-if [ -f webcamp-2015/schedule/schedule.json ]; then
-	git fetch -v --dry-run 2>&1 |
-		grep -qE "\[up\s+to\s+date\]\s+$(
-			git branch 2>/dev/null |
-				sed -n '/^\*/s/^\* //p' |
-					sed -r 's:(\+|\*|\$):\\\1:g'
-		)\s+" || {
-			git pull > /dev/null 2>&1
-			cat schedule.json | ./fix-schedule.py > webcamp-2015/schedule/_schedule.json
-			mv webcamp-2015/schedule/_schedule.json webcamp-2015/schedule/schedule.json
-	}
-else
+
+REMOTE=$(git ls-remote git@github.com:webcampsi/info-beamer.git master | cut -f1)
+LOCAL=$(git rev-parse origin/master)
+
+if [ "${REMOTE}" != "${LOCAL}" ]; then
 	git pull > /dev/null 2>&1
 	cat schedule.json | ./fix-schedule.py > webcamp-2015/schedule/_schedule.json
 	mv webcamp-2015/schedule/_schedule.json webcamp-2015/schedule/schedule.json
