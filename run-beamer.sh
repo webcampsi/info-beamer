7d9cdcb0895816e5a2e41264de730ca368eeacba
Goran Blažič <goranche@me.com>
2015-03-12 22:27:20 +0100

    run git-check when starting up

--- run-beamer.sh
+++ run-beamer.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 [ -f /boot/room ] && cp /boot/room /home/goranb/info-beamer/webcamp-2015/schedule/room
 nohup /opt/info-beamer-pi/info-beamer /home/goranb/info-beamer/webcamp-2015 &
+/home/goranb/info-beamer/check-git.sh
