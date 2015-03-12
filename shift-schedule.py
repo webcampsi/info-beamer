2dfd8bf1e9c9b311a21413e7acd875ad520fe7a0
Jure Cuhalev <gandalfar@gmail.com>
2015-03-12 18:03:26 +0100

    Revert "fix shit-schedule script"
    This reverts commit 26c7fd62f3bc483c11ac2e734b7e61c833150280.

--- shift-schedule.py
+++ shift-schedule.py
@@ -2,14 +2,11 @@ import json
 import arrow
 
 schedule = json.loads(open('webcamp-2015/schedule/schedule.json').read())
-
-date_format = '2015-03-12T{0}:00.0+01:00'
-
 for item in schedule:
-    nice_start = item.get('nice_start')
-    item['start'] = arrow.get( date_format.format(nice_start) ).format('X')
+    start = arrow.get(item.get('start'))
+    item['start'] = start.replace(days=+1).format('X')
     
-    nice_stop = item.get('nice_stop')
-    item['stop'] = arrow.get( date_format.format(nice_stop) ).format('X')
+    end = arrow.get(item.get('end'))
+    item['end'] = end.replace(days=+1).format('X')
 
-print json.dumps(schedule, indent=4)
+print json.dumps(schedule)
