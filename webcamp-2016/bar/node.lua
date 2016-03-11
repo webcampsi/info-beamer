0db14505116aa9f9b762c66e9d0432d049bafed9
Goran Blažič <goranche@me.com>
2016-03-11 18:07:45 +0100

    Revert "Testing automatic update"
    This reverts commit 15ed20d9263bc63e25ae965979f61e363a7c28bf.

--- webcamp-2016/bar/node.lua
+++ webcamp-2016/bar/node.lua
@@ -4,6 +4,6 @@ logo = resource.load_image("wc-logo.png")
 font = resource.load_font("Montserrat-Regular.otf")
 
 function node.render()
-	gl.clear(0, 0, 1, 0.5)
+	gl.clear(1, 1, 1, 0.5)
 	logo:draw(66, 33, 763, 182)
 end
